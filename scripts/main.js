dfede98c9c9a58a2a116dd0df3e9b1d08fcaf8ba
Juraj Lenoch <jurajlenoch@gmail.com>
2021-02-21 19:31:47 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+        if(!localStorage.getItem('name')) {
+            setUserName();
+        } else {
+            let storedName = localStorage.getItem('name');
+            myHeading.textContent = 'Mozilla is cool, ' + storedName;
+        }
+
+        myButton.onclick = function() {
+            setUserName();
+          }
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/title-image.png') {
+      myImage.setAttribute('src','images/journey2.png');
+    } else {
+      myImage.setAttribute('src','images/title-image.png');
+    }
+}
+
